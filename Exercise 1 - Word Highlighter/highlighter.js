b70cf351e9caa95893b08e8b3547946d34ef8cb6
kaitcham <chams4f@gmail.com>
2023-07-24 11:13:35 +0200

    Highlighter.js

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,33 @@
-console.log("Exercise 1 - Word Highlighter");
+document.addEventListener('DOMContentLoaded', () => {
+  const paragraph = document.querySelector('#myParagraph').textContent;
+  const words = paragraph.split(' ');
+  const occurrences = {};
+
+  for (let word of words) {
+    if (!occurrences[word.toLowerCase()]) {
+      occurrences[word.toLowerCase()] = 1;
+    } else {
+      occurrences[word.toLowerCase()]++;
+    }
+  }
+
+  let newParagraph = '';
+
+  const mostFiveOftenWords = Object.fromEntries(
+    Object.entries(occurrences)
+      .sort((a, b) => b[1] - a[1])
+      .slice(1, 6)
+  );
+
+  for (let word of words) {
+    if (mostFiveOftenWords[word.toLowerCase()]) {
+      !word.startsWith(word.charAt(0).toUpperCase())
+        ? (newParagraph += `<span style="background-color: yellow">${word}</span> `)
+        : (newParagraph += `<span style="background-color: yellow; text-decoration: underline">${word}</span> `);
+    } else {
+      newParagraph += `${word} `;
+    }
+  }
+
+  document.querySelector('#myParagraph').innerHTML = newParagraph;
+});
