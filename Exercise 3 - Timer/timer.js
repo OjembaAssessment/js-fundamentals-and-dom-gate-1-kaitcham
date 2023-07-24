7a42fffdbc5ca62840b50d4c94a1e192570bde88
kaitcham <chams4f@gmail.com>
2023-07-24 12:47:10 +0200

    Timer.js

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,35 @@
-console.log("Exercise 3 - Timer");
+let interval = null;
+let count = document.querySelector('#countNumber');
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+document.addEventListener('DOMContentLoaded', () => {
+  count.textContent = randomNumber + 's';
+});
+
+function calculateNewTime(number) {
+  if (interval) return;
+  let newTime = Number(count.textContent.slice(0, -1)) + number;
+  count.textContent = newTime + 's';
+}
+
+function startTimer() {
+  if (interval === null) {
+    event.target.textContent = 'Stop';
+    interval = setInterval(function () {
+      count.textContent = count.textContent.slice(0, -1) - 1 + 's';
+      if (count.textContent === '0s') {
+        clearInterval(interval);
+        interval = null;
+      }
+    }, 1000);
+  } else {
+    event.target.textContent = 'Start';
+    clearInterval(interval);
+    interval = null;
+  }
+}
+
+function resetTimer() {
+  if (interval) return;
+  count.textContent = '0s';
+}
